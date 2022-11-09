505bdcec07bf71605b7c2cb42cb50264f4e57a10
Vaishali IyerÃ <vaishali.k.iyer@gmail.com>
2022-11-09 16:19:58 +0100

    M

--- Vaishali's File.R
+++ Vaishali's File.R
@@ -4,3 +4,14 @@ trends <- read.csv(unzip("trends.zip"))
 trends
 
 trendsBayern <- trends[trends$bundesland == "Freistaat Bayern", ]
+
+
+impf <- read.csv("impfdaten_regional.csv")
+impf
+
+
+impfBayern <- impf[impf$bundesland == "Freistaat Bayern", ]
+impfBayern
+
+
+## Plots
