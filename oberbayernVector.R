1d1a80db038d2ca25fad62cc908256db9cfb82f5
Vaishali IyerÃ <vaishali.k.iyer@gmail.com>
2022-11-15 17:12:07 +0100

    V

---
+++ oberbayernVector.R
@@ -0,0 +1,7 @@
+Oberbayern <- c("SK München", "SK Ingolstadt", "SK Rosenheim","LK Altötting", "LK Berchtesgadener Land", 
+                "LK Bad Tölz-Wolfratshausen", "LK Dachau", "LK Ebersberg", 
+                "LK Eichstätt", "LK Erding", "LK Freising", " LK Fürstenfeldbruck",
+                "LK Garmisch-Partenkirchen", " LK Landsberg a.Lech", "LK Miesbach",
+                "LK Mühldorf a.Inn", "LK München", "LK Neuburg-Schrobenhausen", 
+                "LK Pfaffenhofen a.d.Ilm", "LK Rosenheim", "LK Starnberg", 
+                "LK Traunstein", " LK Weilheim-Schongau", "LK Erding", "LK Bad Reichenhall")
