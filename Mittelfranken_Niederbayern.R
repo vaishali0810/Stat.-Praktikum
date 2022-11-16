84750d808a6d0b95846d3446b0525f2c250100af
BatuhanGK <Batuhan.Guezelkaya@campus.lmu.de>
2022-11-16 16:36:37 +0100

    m

--- Mittelfranken_Niederbayern.R
+++ Mittelfranken_Niederbayern.R
@@ -4,6 +4,13 @@ kreise_mittelfranken<-c("LK Roth", "LK Nürnberger Land",
                        "LK Weißenburg-Gunzenhausen",
                        "LK Erlangen-Höchstadt","LK Fürth",
                        "SK Schwabach","SK Erlangen","SK Ansbach")
+
+kreise_niederbayern<-c("LK Landshut", "SK Landshut", "LK Dingolfing-Landau", 
+                       "LK Freyung-Grafenau", "LK Passau", 
+                       "LK Regen", "LK Deggendorf", "LK Passau",
+                       "SK Passau", "LK Rottal-Inn", "SK Straubing", 
+                       "LK Straubing-Bogen")
+
 kreise_mittelfranken<-as.factor(kreise_mittelfranken)
 h<-rep(0, 1488865)
 h<-as.data.frame(h)
@@ -14,3 +21,5 @@ if(dbayern2$district==i){
   h[i]<-"Mittelfranken"
   }
 }  
+
+#https://www.regierung.niederbayern.bayern.de/regierungsbezirk/landkreise/index.html
