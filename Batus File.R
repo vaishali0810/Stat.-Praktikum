8a198860d6c8535a04bd0859e754854ea2972222
BatuhanGK <Batuhan.Guezelkaya@campus.lmu.de>
2022-11-24 12:54:21 +0100

    m

--- Batus File.R
+++ Batus File.R
@@ -56,8 +56,35 @@ c<-as.data.frame(c)
 colnames(c)[1] <- "date"
 y<-merge(Storage9[[1]],c, by="date",
          all.x=TRUE, all.y=TRUE)
-y
-ggplot(y, aes(date,cases,color = gender,
-              linetype = gender)) +
-  geom_line(stat="identity",size=0.1)+
-  geom_smooth()
+v<-y$cases
+index<-is.na(v)
+v[index]<-0
+y$cases<-v
+
+v<-y$gender
+index<-is.na(v)
+v[index]<-"egal"
+y$gender<-v
+
+y <- y %>% arrange(y, district, date, age_group, cases) %>% 
+  mutate(lag1=lag(cases),
+         lag2=lag(cases,2),
+         lag3=lag(cases,3),
+         lag4=lag(cases,4),
+         lag5=lag(cases,5),
+         lag6=lag(cases,6),
+         lag7=lag(cases,7),
+         lag8=lag(cases,8),
+         lag9=lag(cases,9),
+         lag10=lag(cases,10),
+         lag11=lag(cases,11),
+         lag12=lag(cases,12),
+         lag13=lag(cases,13),
+         lag14=lag(cases,14),
+         MA15cases=(lag1+lag2+lag3+lag4+lag5+lag6+lag7+lag8+lag9+lag10+lag11+lag12+lag13+lag14+cases)/15)
+
+df91<-as.data.frame(y, stringsAsFactors = FALSE)
+ggplot(df91, aes(date,MA15cases,color = gender,
+                 linetype = gender)) +
+  geom_line(size=0.1)
+
