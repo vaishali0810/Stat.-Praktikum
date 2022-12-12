9eb4909cdf1103edda052a0c855d76a57e0cc54f
CLR1997 <colinflinke@gmail.com>
2022-12-12 17:09:26 +0100

    --

--- cases transfo.R
+++ cases transfo.R
@@ -30,11 +30,11 @@ Storage02[[1]]<-unique(Storage02[[1]])
 
 View(Storage02[[1]])
 
-Storage03<-list()
-for(i in 1:length(vector33)){
-  Storage03[[i]]<-Storage01[[i]]%>%group_by(date=cut(date,"week"))%>%
-    summarise(M.A00-A04=sum(M.A00-A04),M.A05-A14=sum(M.A05-A14))
-}
+# Storage03<-list()
+# for(i in 1:length(vector33)){
+#   Storage03[[i]]<-Storage01[[i]]%>%group_by(date=cut(date,"week"))%>%
+#     summarise(M.A00-A04=sum(M.A00-A04),M.A05-A14=sum(M.A05-A14))
+# }
 
 Storage_new<-list()
 for(i in 1:length(vector33)){
@@ -48,3 +48,9 @@ for(i in 1:length(vector33)){
   Storage_new[[i]][,2]<-Storage01[[i]][1,2]
   Storage_new[[i]][,3]<-Storage01[[i]][1,3]
 }
+
+Storage01[[69]] <- Storage01[[69]] %>% 
+  mutate(week = cut.Date(date, breaks = "1 week", labels = FALSE)) %>% 
+  arrange(Order_Date)
+Storage01[[69]]$date <- as.Date(Storage01[[69]]$date)
+str(Storage01[[69]])
