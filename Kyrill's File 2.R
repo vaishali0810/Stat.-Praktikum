1f5d1842fbd90d8e22984946b773713c3f1c4ab3
kyrillguba <kyrill.guba@gmail.com>
2022-12-12 17:02:13 +0100

    m

--- Kyrill's File 2.R
+++ Kyrill's File 2.R
@@ -803,6 +803,11 @@ for(i in 1:length(Storage92)){
 }
 
 ##Impfbayern, popbay
+weekly_cases <- dbayern3 %>%
+  group_by(date = cut(date, "week"))  %>% summarise(case = sum(cases))
+
+weekly_cases$date <- as.Date(weekly_cases$date)
+
 
 vector33<-c(unique(dfcombined$district))
 Storage01<-list()
@@ -814,10 +819,55 @@ View(Storage01[[1]])
 
 library(lubridate)
   
-Storage02<-list()
-for(i in 1:length(Storage01)){
-  
+Storage022<-Storage01
+for(i in 1:length(vector33)){
+  Storage022[[i]]<-Storage01[[i]]%>%group_by(date=unique(cut(dates,"week")))
 }
+
+View(Storage022[[1]])
+
+Storage02[[1]]<-unique(Storage02[[1]])
+
+View(Storage02[[1]])
+
+Storage03<-list()
+for(i in 1:length(vector33)){
+  Storage03[[i]]<-Storage01[[i]]%>%group_by(date=cut(date,"week"))%>%
+    summarise(M.A00-A04=sum(M.A00-A04),M.A05-A14=sum(M.A05-A14))
+}
+
+Storage_new<-list()
+for(i in 1:length(vector33)){
+  Storage_new[[i]]<-data.frame(matrix(NA,ncol=25,nrow=148))
+  colnames(Storage_new[[i]])<-colnames(Storage01[[1]])
+}
+
+View(Storage_new[[1]])
+
+for(i in 1:length(vector33)){
+  Storage_new[[i]][,2]<-Storage01[[i]][1,2]
+  Storage_new[[i]][,3]<-Storage01[[i]][1,3]
+}
+
+subscript1<-unique(cut(dates,"week"))
+subscript2<-levels(subscript1)
+
+
+for(i in 1:length(vector33)){
+  for(j in 1:nrow(Storage_new))
+  Storage_new[[i]][j,4]<-Storage01[[i]][j,4]
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 bezirk_names02<-c(unique(dfcombined$bezirk))
