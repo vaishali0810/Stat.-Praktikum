17180e0795427abd9c1eb3f2e57e553e3d067962
CLR1997 <colinflinke@gmail.com>
2022-12-12 17:59:45 +0100

    whole week data

--- cases transfo.R
+++ cases transfo.R
@@ -1,11 +1,6 @@
 dfcombined <- read.csv("dfcombined.csv", header = TRUE, sep =",")
 View(dfcombined)
 
-# weekly_cases <- dbayern3 %>%
-#   group_by(date = cut(date, "week"))  %>% summarise(case = sum(cases))
-# 
-# weekly_cases$date <- as.Date(weekly_cases$date)
-
 dfcombined$date <- as.Date(dfcombined$date)
 
 dfcombined <- dfcombined %>% 
@@ -15,39 +10,6 @@ dfcombined <- dfcombined %>%
 dfcombined <- dfcombined %>% arrange(district, date)
 View(dfcombined)
 
-vector33<-c(unique(dfcombined$district))
-Storage01<-list()
-for(i in 1:length(vector33)){
-  Storage01[[i]]<-dfcombined[dfcombined$district==vector33[i],]
-}
-View(Storage01[[1]])
-
-
-library(lubridate)
-
-
-
-Storage_new<-list()
-for(i in 1:length(vector33)){
-  Storage_new[[i]]<-data.frame(matrix(NA, ncol = 26, nrow = 148))
-  colnames(Storage_new[[i]])<-colnames(Storage01[[1]])
-}
-
-View(Storage_new[[1]])
-
-for(i in 1:length(vector33)){
-  Storage_new[[i]][, 2] <- Storage01[[i]][1, 2]
-  Storage_new[[i]][, 3] <- Storage01[[i]][1, 3]
-  Storage_new[[i]][, 26] <- c(1:148)
-}
-View(Storage01[[69]])
-
-
-Storage012<-Storage_new
-
-View(Storage012[[1]])
-
-Storage012[[69]]<-Storage01[[69]]%>%select(date,`M.A00-04`,week)
 
 df_comb_week<-dfcombined%>%group_by(district,week)%>%summarise(M.A00.04 =sum(M.A00.04),
                                                                     M.A05.14 = sum(M.A05.14),
@@ -72,9 +34,9 @@ df_comb_week<-dfcombined%>%group_by(district,week)%>%summarise(M.A00.04 =sum(M.A
                                                                     Unb.Aunb = sum(Unb.Aunb),
                                                                     total_cases = sum(total_cases),
                                                                     .groups="keep")
-identical(sum(dfcombined$total_cases), sum(df_comb_week$total_cases))
+# identical(sum(dfcombined$total_cases), sum(df_comb_week$total_cases))
 
-View(Storage012[[69]])
+
 
 
 
