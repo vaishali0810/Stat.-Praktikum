25c08b8a82580486c60e59c3bcb67a349f492305
CLR1997 <colinflinke@gmail.com>
2022-12-21 18:45:37 +0100

    ---

--- models.R
+++ models.R
@@ -27,6 +27,20 @@ re1 <- plm(total_cases~lag(total_cases, 7), data= testpdf, model = "random")
 
 
 
+rm(list=ls())
+library(plm) 
+dfultimate <- read.csv("dfultimate.csv", header = TRUE, sep = ",")
+df <- dfultimate %>% select(district, week, bezirk, inzidenz, density, m_anteil, f_anteil, M.A00.04.Anteil, M.A05.14.Anteil,
+                            M.A15.34.Anteil, M.A35.59.Anteil, M.A60.79.Anteil, M.A80.Anteil, M.Aunb.Anteil, F.A00.04.Anteil,
+                            F.A05.14.Anteil, F.A15.34.Anteil, F.A35.59.Anteil, F.A60.79.Anteil, F.A80.Anteil, F.Aunb.Anteil,
+                            A00.04.Anteil, A05.14.Anteil, A15.34.Anteil, A35.59.Anteil, A60.79.Anteil, A80.Anteil,
+                            M.Anteil, F.Anteil, rate_erstimpf, rate_zweitimpf, rate_drittimpf, rate_viertimpf,
+                            weightednbinz, unweightednbinz)
+
+
+df_pan <- pdata.frame(df, index=c("district", "week"))
+
+
 dfultimate_pan <- pdata.frame(dfultimate, index=c("district", "week"))
 
 
@@ -79,6 +93,114 @@ plot(residuals(re3))
 
 
 
+re10 <- plm(inzidenz ~ lag(inzidenz, 1) + lag(weightednbinz, 1)  + rate_zweitimpf + rate_drittimpf
+            - 1, data =df_pan, model = "random")
+
+summary(re10)
+
+re11 <-plm(inzidenz ~ lag(inzidenz, 1) 
+           - 1, data =df_pan, model = "random")
+
+summary(re11)
+
+re12 <- plm(inzidenz ~ lag(inzidenz, 1) + lag(weightednbinz, 1) + A00.04.Anteil + A05.14.Anteil+ A15.34.Anteil
+            + A35.59.Anteil + A60.79.Anteil + A80.Anteil,
+            - 1, data =df_pan, model = "random")
+
+summary(re12)
+
+re13 <- plm(inzidenz ~ lag(inzidenz, 1) + lag(weightednbinz, 1)  + A05.14.Anteil+ A15.34.Anteil
+            + A60.79.Anteil,
+            - 1, data =df_pan, model = "random")
+
+
+
+summary(re13)
+
+
+re14 <- plm(inzidenz ~ lag(inzidenz, 1) + lag(unweightednbinz, 1)  + A05.14.Anteil+ A15.34.Anteil
+            + A60.79.Anteil,
+            - 1, data =df_pan, model = "random")
+summary(re14)
+
+#..
+
+
+
+p<-c(1:9)
+nullt<-subset(df,dfultimate$Kalendarwoche%in%p)
+
+p<-c(10:20)
+erst<-subset(df,dfultimate$Kalendarwoche%in%p)
+
+p<-c(21:39)
+zweit<-subset(df,dfultimate$Kalendarwoche%in%p)
+
+p<-c(40:(52+8))
+dritt<-subset(df,dfultimate$Kalendarwoche%in%p)
+
+p<-c((52+9):(52+23))
+viert<-subset(df,dfultimate$Kalendarwoche%in%p)
+
+p<-c((52+24):(52+30))
+fünft<-subset(df,dfultimate$Kalendarwoche%in%p)
+
+p<-c((52+31):(52+51))
+sechst<-subset(df,dfultimate$Kalendarwoche%in%p)
+
+p<-c((52+52):(52+151))
+siebt<-subset(df,dfultimate$Kalendarwoche%in%p)
+
+p<-c(21:30)
+zweit_a<-subset(df,dfultimate$Kalendarwoche%in%p)
+
+p<-c(31:39)
+zweit_b<-subset(df,dfultimate$Kalendarwoche%in%p)
+
+p<-c((52+31):(52+39))
+sechst_a<-subset(df,dfultimate$Kalendarwoche%in%p)
+
+p<-c((52+40):(52+51))
+sechst_a<-subset(dfultimate,dfultimate$Kalendarwoche%in%p)
+
+nullt <- pdata.frame(nullt, index=c("district", "week"))
+
+re15 <- plm(inzidenz ~ lag(inzidenz, 1) + lag(weightednbinz, 1)  + A05.14.Anteil+ A15.34.Anteil
+            + A60.79.Anteil,
+            - 1, data =nullt, model = "random")
+
+
+
+summary(re15)
+
+sechst <- pdata.frame(sechst, index=c("district", "week"))
+re16 <- plm(inzidenz ~ lag(inzidenz, 1) + lag(weightednbinz, 1)  + A05.14.Anteil+ A15.34.Anteil
+            + A60.79.Anteil + rate_zweitimpf,
+            - 1, data =sechst, model = "random")
+summary(re16)
+
+
+
+fünft <- pdata.frame(fünft, index=c("district", "week"))
+re17 <- plm(inzidenz ~ lag(inzidenz, 1) + lag(weightednbinz, 1)  + A05.14.Anteil+ A15.34.Anteil
+            + A60.79.Anteil + rate_zweitimpf,
+            - 1, data =fünft, model = "random")
+summary(re17)
+
+
+
+
+siebt<-pdata.frame(siebt, index=c("district", "week"))
+
+re18 <- plm(inzidenz ~ lag(inzidenz, 1) + lag(weightednbinz, 1) + lag(inzidenz,2) + lag(weightednbinz, 2) + A05.14.Anteil+ A15.34.Anteil
+            + A60.79.Anteil + rate_zweitimpf,
+            - 1, data =siebt, model = "random")
+summary(re18)
+
+re19 <- plm(inzidenz ~ lag(inzidenz, 1) + lag(unweightednbinz, 1)  + A05.14.Anteil+ A15.34.Anteil
+            + A60.79.Anteil + rate_zweitimpf,
+            - 1, data =siebt, model = "random")
+summary(re19)
 
 
 
@@ -87,7 +209,15 @@ plot(residuals(re3))
 
 
 
+re100 <- plm(inzidenz ~ lag(inzidenz, 1) + lag(weightednbinz, 1) + lag(inzidenz,2) + lag(weightednbinz, 2)
+            + A00.04.Anteil + A05.14.Anteil+ A15.34.Anteil + M.Anteil +density
+            + A35.59.Anteil + A60.79.Anteil + A80.Anteil,
+            - 1, data =df_pan, model = "random")
 
+
+
+stepAIC(object = re100, direction = "both",
+        k = log(nrow(df_pan)), trace = FALSE)
 
 
 
