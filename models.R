fea5fe1db69fcc40662353ea77736b7e0b33c3ff
BatuhanGK <Batuhan.Guezelkaya@campus.lmu.de>
2022-12-22 18:07:05 +0100

    m

--- models.R
+++ models.R
@@ -142,7 +142,7 @@ p<-c((52+31):(52+39))
 sechst_a<-subset(df,dfultimate$Kalendarwoche%in%p)
 
 p<-c((52+40):(52+51))
-sechst_a<-subset(dfultimate,dfultimate$Kalendarwoche%in%p)
+sechst_b<-subset(dfultimate,dfultimate$Kalendarwoche%in%p)
 
 nullt <- pdata.frame(nullt, index=c("district", "week"))
 
