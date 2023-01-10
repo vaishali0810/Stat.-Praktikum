7584bd7cd2cf07ce13f9bd38971fb63f80f7a281
CLR1997 <colinflinke@gmail.com>
2023-01-10 15:32:04 +0100

    ---

--- Colin models.R
+++ Colin models.R
@@ -49,4 +49,48 @@ phtest(re100, fe1)
 
 
 
+fe1.fixedweek <- plm(inzidenz ~ lag(inzidenz, 1) + lag(weightednbinz, 1) + lag(inzidenz,2) + lag(weightednbinz, 2) +
+                       A00.04.Anteil + A05.14.Anteil+ A15.34.Anteil + M.Anteil 
+                     + A35.59.Anteil + A60.79.Anteil + A80.Anteil + rate_zweitimpf + factor(week)
+                     , data =df_pan, model = "within")
+summary(fe1.fixedweek)
+pFtest(fe1.fixedweek, fe1)
+plmtest(fe1, c("time"), type=("bp"))
 
+## fixed time effect week is needed, both clearly smaller than 0.05
+
+pcdtest(fe1, test = c("lm"))
+pcdtest(fe1, test = c("cd"))
+pbgtest(fe1)
+
+## cross sectional dependancy, is obvious given neighboring districts are part of the independent variables
+## serial correlation at hand
+
+library(tseries)
+adf.test(df_pan$inzidenz, k=2)
+
+## pvalue 0.01 --> stationary for lag order 2 
+## This also holds for k=1, k=3, k=4
+## k meaning biggest lag level
+
+bptest(fe1.fixedweek, data = df_pan, studentize=F)
+
+## heteroskedasticity at hand
+
+
+t(sapply(c("HC0", "HC1", "HC2", "HC3", "HC4"), function(x) sqrt(diag(vcovHC(fe1, type = x)))))
+
+coeftest(fe1, vcovHC(fe1, method = "arellano"))
+
+fe1.vcovHC <- vcovHC(fe1, method = "arellano")
+
+acf(fe1$residuals, type = "correlation")
+### link is both lines
+### https://www.codingprof.com/3-easy-ways-to-test-for-autocorrelation-in-r-examples/#:~:
+### text=In%20R%2C%20the%20easiest%20way%20to%20test%20for,perform%20the%20Durbin-Watson%20test%20or%20the%20Breusch-Godfrey%20test.
+fe1.HC <- coeftest(fe1, vcovHC)
+summary(fe1.HC)
+fe1.HC$residuals
+residuals.plm$residuals.plm
+
+### ---> arellano
