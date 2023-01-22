a28176a397c47f81c316cf6e8226f3233bb27c6d
BatuhanGK <Batuhan.Guezelkaya@campus.lmu.de>
2023-01-22 10:49:06 +0100

    m

--- plot effects.R
+++ plot effects.R
@@ -169,48 +169,48 @@ plot(a(h), xlab = "Hotspot", ylab = "Effekte") + title("Effekt für Hotspot mit 
 summary(df4_pan$density)
 h<-c(66.0:4788.0)
 a<-function(x){(-0.0284347)*sqrt(log(x)*1000)}
-plot(a(h), xlab = "Dichte", ylab = "Effekte") + title("Effekt bei Inzidenz=1000 für sqrt-Modell")
+plot(a(h), xlab = "Dichte", ylab = "Effekte") + title("Effekt bei Inzidenz=1000 sqrt-Modell")
 
 summary(df4_pan$weightednbinz)
 h<-c(0.0:3302.4)
 a<-function(x){(0.2316856)*sqrt(log(x)*1000)}
-plot(a(h), xlab = "gewichtete Nachbar-Inzidenz", ylab = "Effekte") + title("Effekt für gewichtete Nachbar-Inzidenz mit Inzidenz = 1000 für sqrt-Modell")
+plot(a(h), xlab = "gewichtete Nachbar-Inzidenz", ylab = "Effekte") + title("Effekt für gewichtete Nachbar-Inzidenz mit Inzidenz = 1000 sqrt-Modell")
 
 summary(df4_pan$hotspot)
 h<-c(0.0:3302.4)
 a<-function(x){(0.2275481)*sqrt(log(x)*1000)}
-plot(a(h), xlab = "Hotspot", ylab = "Effekte") + title("Effekt für Hotspot mit Inzidenz = 1000 für sqrt-Modell")
+plot(a(h), xlab = "Hotspot", ylab = "Effekte") + title("Effekt für Hotspot mit Inzidenz = 1000 sqrt-Modell")
 
 ########## 100
 summary(df4_pan$density)
 h<-c(66.0:4788.0)
 a<-function(x){(-0.0284347)*sqrt(log(x)*100)}
-plot(a(h), xlab = "Dichte", ylab = "Effekte") + title("Effekt für Dichte mit Inzidenz = 100 für sqrt-Modell")
+plot(a(h), xlab = "Dichte", ylab = "Effekte") + title("Effekt für Dichte mit Inzidenz = 100 sqrt-Modell")
 
 summary(df4_pan$weightednbinz)
 h<-c(0.0:3302.4)
 a<-function(x){(0.2316856)*sqrt(log(x)*100)}
-plot(a(h), xlab = "gewichtete Nachbar-Inzidenz", ylab = "Effekte") + title("Effekt für gewichtete Nachbar-Inzidenz mit Inzidenz = 100 für sqrt-Modell")
+plot(a(h), xlab = "gewichtete Nachbar-Inzidenz", ylab = "Effekte") + title("Effekt für gewichtete Nachbar-Inzidenz mit Inzidenz = 100 sqrt-Modell")
 
 summary(df4_pan$hotspot)
 h<-c(0.0:3302.4)
 a<-function(x){(0.2275481)*sqrt(log(x)*100)}
-plot(a(h), xlab = "Hotspot", ylab = "Effekte") + title("Effekt für Hotspot mit Inzidenz = 100 für sqrt-Modell")
+plot(a(h), xlab = "Hotspot", ylab = "Effekte") + title("Effekt für Hotspot mit Inzidenz = 100 sqrt-Modell")
 
 ########## 10
 summary(df4_pan$density)
 h<-c(66.0:4788.0)
 a<-function(x){(-0.0284347)*sqrt(log(x)*10)}
-plot(a(h), xlab = "Dichte", ylab = "Effekte") + title("Effekt für Dichte mit Inzidenz = 10 für sqrt-Modell")
+plot(a(h), xlab = "Dichte", ylab = "Effekte") + title("Effekt für Dichte mit Inzidenz = 10 sqrt-Modell")
 
 summary(df4_pan$weightednbinz)
 h<-c(0.0:3302.4)
 a<-function(x){(0.2316856)*sqrt(log(x)*10)}
-plot(a(h), xlab = "gewichtete Nachbar-Inzidenz", ylab = "Effekte") + title("Effekt für gewichtete Nachbar-Inzidenz mit Inzidenz = 10 für sqrt-Modell")
+plot(a(h), xlab = "gewichtete Nachbar-Inzidenz", ylab = "Effekte") + title("Effekt für gewichtete Nachbar-Inzidenz mit Inzidenz = 10 sqrt-Modell")
 
 summary(df4_pan$hotspot)
 h<-c(0.0:3302.4)
 a<-function(x){(0.2275481)*sqrt(log(x)*10)}
-plot(a(h), xlab = "Hotspot", ylab = "Effekte") + title("Effekt für Hotspot mit Inzidenz = 10 für sqrt-Modell")
+plot(a(h), xlab = "Hotspot", ylab = "Effekte") + title("Effekt für Hotspot mit Inzidenz = 10 sqrt-Modell")
 
 #
