9c543a491059d1c52ba8cfe53e0b68a4e8374fcc
CLR1997 <colinflinke@gmail.com>
2023-01-24 19:07:26 +0100

    --

--- Colin models.R
+++ Colin models.R
@@ -877,11 +877,11 @@ coeftest(pool, vcovHC(pool, type = "HC0"))
 
 
 
-inzidenz ~ lag(inzidenz, 1) + lag(weightednbinz, 1) 
-+ I(log(density)*lag(inzidenz, 1)) + I(hotspot * lag(inzidenz, 1)) 
-+I(hotspotnb * lag(weightednbinz, 1)) + I(rate_zweitimpf * hotspot) 
-+ A60.79.Anteil 
-+ factor(week)
+# inzidenz ~ lag(inzidenz, 1) + lag(weightednbinz, 1) 
+# + I(log(density)*lag(inzidenz, 1)) + I(hotspot * lag(inzidenz, 1)) 
+# +I(hotspotnb * lag(weightednbinz, 1)) + I(rate_zweitimpf * hotspot) 
+# + A60.79.Anteil 
+# + factor(week)
 
 
 fgls1 <- pggls(formula = inzidenz ~ lag(inzidenz, 1) + lag(weightednbinz, 1) 
