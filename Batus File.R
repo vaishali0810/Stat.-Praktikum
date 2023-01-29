d141bbac2dc136f19ec767d8cb5c4642b0f86525
BatuhanGK <Batuhan.Guezelkaya@campus.lmu.de>
2023-01-29 16:09:48 +0100

    m

--- Batus File.R
+++ Batus File.R
@@ -17,9 +17,42 @@ pool.weighted <- plm(inzidenz ~ lag(inzidenz, 1) + lag(weightednbinz, 1)
                      + factor(week)
                      , data =df4_pan,weights = 1/(sqrt(inzidenz + 1)), model = "pooling")
 library(stargazer)
-stargazer(pool,pool.sqrt.actual,pool.weighted)
+
+pool.sqrt.actual.lag <- plm(sqrt(inzidenz) ~ sqrt(lag(inzidenz, 1)) + sqrt(lag(weightednbinz, 1))
+                            + I(log(density)*sqrt(lag(inzidenz, 1))) + I(hotspot * sqrt(lag(inzidenz, 1))) 
+                            + I(hotspotnb * sqrt(lag(weightednbinz, 1))) + I(lag(rate_zweitimpf,1) * hotspot)
+                            + lag(A60.79.Anteil, 1)
+                            + factor(week)
+                            , data =df4_pan, model = "pooling")
+
+stargazer(pool.sqrt.actual.lag)
 
 a<-coeftest(pool, vcovHC(pool, type = "HC0"))
 b<-coeftest(pool.sqrt.actual, vcovHC(pool.sqrt.actual, type = "HC0"))
 c<-summary(pool.weighted)
 stargazer(b)
+
+sum.sqrt.nullt <- summary(pool.sqrt.nullt)
+sum.sqrt.erst <- summary(pool.sqrt.erst)
+sum.sqrt.zweit <- summary(pool.sqrt.zweit)
+sum.sqrt.dritt <-summary(pool.sqrt.dritt)
+sum.sqrt.viert <-summary(pool.sqrt.viert)
+sum.sqrt.fuenft <-summary(pool.sqrt.fuenft)
+sum.sqrt.sechst <-summary(pool.sqrt.sechst)
+sum.sqrt.siebt <-summary(pool.sqrt.siebt)
+
+a<-list(sum.sqrt.nullt, 
+sum.sqrt.erst,
+sum.sqrt.zweit, 
+sum.sqrt.dritt,
+sum.sqrt.viert,
+sum.sqrt.fuenft,
+sum.sqrt.sechst,
+sum.sqrt.siebt)
+
+b<-list()
+for (i in seq_along(a)){
+  b[i]<-a[[i]]
+}
+
+
